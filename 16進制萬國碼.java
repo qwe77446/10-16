267b5ab8f43973d333f98002b1bf2cafff5060f0
qwe77446 <43276241+qwe77446@users.noreply.github.com>
2018-10-17 09:08:57 +0800

    Add files via upload

---
+++ 16進制萬國碼.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String c2 = scn.next();
+        int v1 = Integer.valueOf(c2,16);
+        System.out.println((char)v1);
+
+
+    }
+}
